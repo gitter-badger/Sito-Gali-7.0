21acff056f785ea07ef8a9fe0b2894f9a9f467b3
Giulio Seregni <giulio.seregni@pingendo.com>
2016-02-03 13:12:27 +0100

    script costruzioni

---
+++ trello-connect.js
@@ -0,0 +1,38 @@
+ $(window).on("ready", function() {
+
+  $.getJSON( "./trello.json", function( data ) {
+    console.log(data);
+    var items = [];
+    
+    var idList = null;
+    // find the id list
+    $.each(data.lists, function( key, list ) {
+      if (list.name == document.location.pathname.substr(1)) {
+        idList = list.id;
+        return false
+      }
+    });
+
+    if (!idList)
+      return;
+
+    var $model = $(".trello");
+
+    $.each(data.cards, function( key, card ) {
+      if (card.idList != idList)
+        return;
+      // we have a card
+      // find attachment
+      var $clone = $model.clone();
+      var url = card.attachments[0].url.replace("https://trello-attachments.s3.amazonaws.com", "http://galimberti.imgix.net");
+      url = url + "?w=1140";
+      $clone.find("img").attr("src", url);
+      $clone.appendTo($model.parent());
+
+    });
+   
+   $model.remove();
+    
+  });
+
+});
