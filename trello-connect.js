5636dcf98c953466c0e3c05d915c5a0f42cc1e55
Giulio Seregni <giulio.seregni@pingendo.com>
2016-02-24 15:20:11 +0100

    more

--- trello-connect.js
+++ trello-connect.js
@@ -251,7 +251,7 @@ $(window).on("ready", function() {
       $wrapper.append($img);
       
       $wrapper.find(".fa-facebook").on("click", function() {
-        alert("3 jjk");
+        alert("34 jjk");
         FB.ui(
         {
           method: 'feed',
