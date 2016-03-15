63f5e4eafd50ef438a6cd59f8c775b24aef79824
ggali <gabriele.galimberti@pingendo.com>
2016-03-15 12:29:03 +0100

    a

---
+++ Sito 5.0 pulito x Github/it/js/ie.js
@@ -0,0 +1,9 @@
+jQuery(window).load(function(){    
+    	jQuery('#addr').corner("round 8px cc:#FFF");
+    	jQuery('.social').corner("round 8px cc:#FFF");    	
+    	jQuery('.span4 .btn').append('<div class="btnFooter"></div>');	 	           	
+    	jQuery('.span5 #btnpdf2').append('<div class="btnFooter"></div>');	     	
+	jQuery('.innercontent').append('<div class="footerCnt"></div>');
+	jQuery('.btn-group-vertical').prepend('<div class="vert_top"></div>');
+	jQuery('.btn-group-vertical').append('<div class="vert_footer"></div>');	
+});
