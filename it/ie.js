1116f3b616471f19a79e6d2a13ad83a796dd7c5e
ggali <gabriele.galimberti@pingendo.com>
2016-03-15 12:57:14 +0100

    a

--- it/ie.js
+++
@@ -1,8 +0,0 @@
-jQuery(window).load(function(){    
-    	jQuery('#addr').corner("round 8px cc:#FFF");
-    	jQuery('.social').corner("round 8px cc:#FFF");    	
-    	jQuery('.span4 .btn').append('<div class="btnFooter"></div>');	 	           	
-	jQuery('.innercontent').append('<div class="footerCnt"></div>');
-	jQuery('.btn-group-vertical').prepend('<div class="vert_top"></div>');
-	jQuery('.btn-group-vertical').append('<div class="vert_footer"></div>');	
-});
